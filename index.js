ceebdcc8986b64c7bb75af46fa6ec338236cc96a
appl4e <appl4e@gmail.com>
2025-07-27 13:04:34 +0600

    writting  to files in json format to store as data

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const http = require("http");
 const { handleReqRes } = require("./helpers/handleReqRes");
 const environmentVariable = require("./helpers/environments");
+const { create } = require("./lib/data");
 
 const app = {};
 
 // app.config = {
 //   port: environmentVariable.port,
 // };
+
+// testing file creation
+create(
+  "test",
+  "text",
+  { name: "Apple Mahmood", phone: "01678116782", email: "appl4e@gmail.com" },
+  (error) => {
+    console.log(error);
+  }
+);
 
 app.createServer = () => {
   const server = http.createServer(app.handleRequestResponse);
