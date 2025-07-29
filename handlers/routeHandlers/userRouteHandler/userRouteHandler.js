cc8b34f55b8e349816c94bbb078fa791ba76e120
appl4e <appl4e@gmail.com>
2025-07-29 16:32:04 +0600

    working on users post api

--- handlers/routeHandlers/userRouteHandler/userRouteHandler.js
+++ handlers/routeHandlers/userRouteHandler/userRouteHandler.js
@@ -13,7 +13,43 @@ handler.userRouteHandler = (reqResProperties, callback) => {
 
 handler._users = {};
 
-handler._users.post = (reqResProperties, callback) => {};
+handler._users.post = (reqResProperties, callback) => {
+  const firstName =
+    typeof reqResProperties.body.firstName === "string" &&
+    reqResProperties.body.firstName.trim().length > 0
+      ? reqResProperties.body.firstName
+      : false;
+
+  const lastName =
+    typeof reqResProperties.body.lastName === "string" &&
+    reqResProperties.body.lastName.trim().length > 0
+      ? reqResProperties.body.lastName
+      : false;
+
+  const email =
+    typeof reqResProperties.body.email === "string" &&
+    reqResProperties.body.email.trim().length > 0
+      ? reqResProperties.body.email
+      : false;
+
+  const phone =
+    typeof reqResProperties.body.phone === "string" &&
+    reqResProperties.body.phone.trim().length == 11
+      ? reqResProperties.body.phone
+      : false;
+
+  const password =
+    typeof reqResProperties.body.password === "string" &&
+    reqResProperties.body.password.trim().length == 11
+      ? reqResProperties.body.password
+      : false;
+
+  const tosAgreement =
+    typeof reqResProperties.body.tosAgreement === "string" &&
+    reqResProperties.body.tosAgreement.trim().length == 11
+      ? reqResProperties.body.tosAgreement
+      : false;
+};
 handler._users.put = (reqResProperties, callback) => {};
 handler._users.get = (reqResProperties, callback) => {
   callback(200, { message: "this is get users api" });
