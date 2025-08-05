5303ea5504f9c517c5d4efec3751a978fe0c74d3
appl4e <appl4e@gmail.com>
2025-08-05 12:26:36 +0600

    user update and delete method developed

--- handlers/routeHandlers/userRouteHandler/userRouteHandler.js
+++ handlers/routeHandlers/userRouteHandler/userRouteHandler.js
@@ -81,7 +81,75 @@ handler._users.post = (reqResProperties, callback) => {
     callback(422, { error: "User data validation Error" });
   }
 };
-handler._users.put = (reqResProperties, callback) => {};
+handler._users.put = (reqResProperties, callback) => {
+  const firstName =
+    typeof reqResProperties.body.firstName === "string" &&
+    reqResProperties.body.firstName.trim().length > 0
+      ? reqResProperties.body.firstName
+      : false;
+
+  const lastName =
+    typeof reqResProperties.body.lastName === "string" &&
+    reqResProperties.body.lastName.trim().length > 0
+      ? reqResProperties.body.lastName
+      : false;
+
+  const email =
+    typeof reqResProperties.body.email === "string" &&
+    reqResProperties.body.email.trim().length > 0
+      ? reqResProperties.body.email
+      : false;
+
+  const phone =
+    typeof reqResProperties.body.phone === "string" &&
+    reqResProperties.body.phone.trim().length == 11
+      ? reqResProperties.body.phone
+      : false;
+
+  const password =
+    typeof reqResProperties.body.password === "string" &&
+    reqResProperties.body.password.trim().length > 0
+      ? reqResProperties.body.password
+      : false;
+
+  if (phone) {
+    if (firstName || lastName || password) {
+      data.read("users", phone, (readErr, uData) => {
+        if (!readErr && uData) {
+          const userData = { ...parseJson(uData) };
+          if (firstName) {
+            userData.firstName = firstName;
+          }
+          if (lastName) {
+            userData.lastName = lastName;
+          }
+          if (password) {
+            userData.password = hashString(password);
+          }
+          data.update("users", phone, userData, (updateErr) => {
+            if (!updateErr) {
+              callback(200, {
+                message: "The user data has been updated successfully.",
+              });
+            } else {
+              callback(400, {
+                error: "There was a problem updating the user data.",
+              });
+              console.log(updateErr);
+            }
+          });
+        } else {
+          callback(404, { error: "User data not found." });
+          console.log(readErr);
+        }
+      });
+    } else {
+      callback(400, { error: "There was a problem with your request." });
+    }
+  } else {
+    callback(400, { error: "There was a problem with your phone number." });
+  }
+};
 handler._users.get = (reqResProperties, callback) => {
   const phone =
     typeof reqResProperties.queryStringObject.phone === "string" &&
@@ -103,6 +171,33 @@ handler._users.get = (reqResProperties, callback) => {
     callback(404, { error: "user data with this query is not found." });
   }
 };
-handler._users.delete = (reqResProperties, callback) => {};
+handler._users.delete = (reqResProperties, callback) => {
+  const phone =
+    typeof reqResProperties.queryStringObject.phone === "string" &&
+    reqResProperties.queryStringObject.phone.trim().length == 11
+      ? reqResProperties.queryStringObject.phone
+      : false;
+  console.log(typeof reqResProperties.queryStringObject.phone);
+
+  if (phone) {
+    data.read("users", phone, (readErr, uData) => {
+      if (!readErr) {
+        data.remove("users", phone, (remErr) => {
+          if (!remErr) {
+            callback(200, { message: "The user removed successfully" });
+          } else {
+            callback(500, { error: "There was a error removing the user." });
+            console.log(remErr);
+          }
+        });
+      } else {
+        callback(400, { error: "unable to find user with this phone number." });
+        console.log(readErr);
+      }
+    });
+  } else {
+    callback(400, { error: "there was a problem in your request." });
+  }
+};
 
 module.exports = handler;
